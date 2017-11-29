c93ac05e8946132988be421d725f385b3e089fe7
tusmanakhter <tusman96@hotmail.com>
2017-11-28 23:38:42 -0500

    Updated advertisement create form

--- application/views/advertisements/create.php
+++ application/views/advertisements/create.php
@@ -16,17 +16,31 @@
                     </optgroup>
                 </select>
                 <div class="clearfix"></div>
-                <label>Ad Title <span>*</span></label>
-                <input type="text" class="phone" placeholder="">
+                <label>Title <span>*</span></label>
+                <input type="text" class="phone" placeholder="Title">
                 <div class="clearfix"></div>
-                <label>Ad Description <span>*</span></label>
+                <label>Description <span>*</span></label>
                 <textarea class="mess" placeholder="Write few lines about your product"></textarea>
                 <div class="clearfix"></div>
+                <label>Price <span>*</span></label>
+                <input type="text" class="price" placeholder="Price">
+                <div class="clearfix"></div>
+                <label>Type <span>*</span></label>
+                <select>
+                    <option value="Buy">Buy</option>
+                    <option value="Sell">Sell</option>
+                </select>
+                <div class="clearfix"></div>
+                <label>For Sale By <span>*</span></label>
+                <select>
+                    <option value="Owner">Owner</option>
+                    <option value="Business">Business</option>
+                </select>
+                <div class="clearfix"></div>
             <div class="upload-ad-photos">
-            <label>Photos for your ad :</label>	
+            <label>Images:</label>	
                 <div class="photos-upload-view">
                     <form id="upload" action="index.html" method="POST" enctype="multipart/form-data">
-
                     <input type="hidden" id="MAX_FILE_SIZE" name="MAX_FILE_SIZE" value="300000" />
 
                     <div>
@@ -37,32 +51,27 @@
                     <div id="submitbutton">
                         <button type="submit">Upload Files</button>
                     </div>
-
                     </form>
-
-                    <div id="messages">
-                    <p>Status Messages</p>
-                    </div>
                     </div>
                 <div class="clearfix"></div>
                     <script src="js/filedrag.js"></script>
             </div>
                 <div class="personal-details">
                 <form>
-                    <label>Your Name <span>*</span></label>
-                    <input type="text" class="name" placeholder="">
-                    <div class="clearfix"></div>
-                    <label>Your Mobile No <span>*</span></label>
-                    <input type="text" class="phone" placeholder="">
+                    <label>Your Phone # <span>*</span></label>
+                    <input type="text" class="phone" placeholder="Phone number">
                     <div class="clearfix"></div>
                     <label>Your Email Address<span>*</span></label>
-                    <input type="text" class="email" placeholder="">
+                    <input type="text" class="email" placeholder="Email Address">
+                    <div class="clearfix"></div>
+                    <label>Selling Address<span>*</span></label>
+                    <input type="text" class="email" placeholder="Address">
                     <div class="clearfix"></div>
                     <p class="post-terms">By clicking <strong>post Button</strong> you accept our <a href="terms.html" target="_blank">Terms of Use </a> and <a href="privacy.html" target="_blank">Privacy Policy</a></p>
                 <input type="submit" value="Post">					
                 <div class="clearfix"></div>
                 </form>
-                </div>
+            </div>
         </div>
     </div>	
 </div>
