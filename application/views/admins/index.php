b5be439ec43b728a440ec97c43a69e5795a5ab9c
Laxman <laxman_20@hotmail.com>
2017-12-01 03:27:34 -0500

    Fix buttons

--- application/views/admins/index.php
+++ application/views/admins/index.php
@@ -1,8 +1,27 @@
-<div class="container">
-    <div class="text-center">
-        <a href="<?php echo base_url();?>admins/"><button type="button" >Advertisements</button></a>
-        <a href="<?php echo base_url();?>admins/payments"><button type="button" >Payments</button></a>
-        <a href="<?php echo base_url();?>admins/reports"><button type="button" >Reports</button></a>
-
+<div class="content">
+    <div class="categories">
+        <div class="container">
+            <div class="col-md-3 focus-grid">
+                <a href="<?php echo base_url();?>admins/payments">
+                <div class="focus-border">
+                        <div class="focus-layout">
+                            <div class="focus-image"><i class="fa fa-asterisk"></i></div>
+                            <h4 class="clrchg">Payments</h4>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 focus-grid">
+                <a href="<?php echo base_url();?>admins/reports">
+                    <div class="focus-border">
+                        <div class="focus-layout">
+                            <div class="focus-image"><i class="fa fa-asterisk"></i></div>
+                            <h4 class="clrchg">Reports</h4>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="clearfix"></div>
+        </div>
     </div>
 </div>
