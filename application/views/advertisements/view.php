cc6c40e4766f1ba2f5bf3e3f5b283f2914a52d19
tusmanakhter <tusman96@hotmail.com>
2017-12-04 19:29:44 -0500

    Display type

--- application/views/advertisements/view.php
+++ application/views/advertisements/view.php
@@ -12,6 +12,7 @@
             Date: <?php echo $advertisement['date'] ?></br>
             Address: <?php echo $advertisement['address'] ?></br>
             Availability: <?php echo $advertisement['availability'] ?></br>
+            Type: <?php echo $advertisement['type'] ?></br>
             StoreId: <?php echo $advertisement['storeId']; ?></br>
             Rating: <?php echo $advertisement['rating'] ?></br>
             Email:<?php echo $advertisement['email'] ?></br>
